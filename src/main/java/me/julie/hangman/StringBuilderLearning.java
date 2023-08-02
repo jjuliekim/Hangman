b31997ada5f10751f2a1ad91e103788c662066bc
jjuliekim <118621382+jjuliekim@users.noreply.github.com>
2023-08-02 17:10:03 +0900

    initial commit

--- src/main/java/me/julie/hangman/StringBuilderLearning.java
+++
@@ -1,9 +0,0 @@
-package me.julie.hangman;
-
-public class StringBuilderLearning {
-    public static void main(String[] args) {
-        Chair chair1 = new Chair(4);
-        Chair chair2 = new Chair(4);
-        System.out.println('c' == 'c');
-    }
-}
